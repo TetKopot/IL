7d3ba2161a1c9ceccf8cca8b39ecc2020c7ddfe6
TetKopot <tet.kopot@gmail.com>
2023-07-31 23:43:42 +0300

    add changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,6 +6,26 @@ document.addEventListener('DOMContentLoaded', () => {
   const pageBody = document.querySelector('.page__body');
   const menuItems = document.querySelectorAll('.menu__item');
 
+  const switchInputs = document.querySelectorAll('.switch-wrapper input');
+  const prices = document.querySelectorAll('.price');
+  const toggleClass = 'hide';
+
+  for (const switchInput of switchInputs) {
+    switchInput.addEventListener('input', function() {
+      for (const price of prices) {
+        price.classList.add(toggleClass);
+      }
+
+      const activePrices = document.querySelectorAll(
+        `.price.${switchInput.id}`
+      );
+
+      for (const activePrice of activePrices) {
+        activePrice.classList.remove(toggleClass);
+      }
+    });
+  }
+
   menuButton.addEventListener('click', () => {
     menu.classList.toggle('active');
     menuButton.classList.toggle('active');
